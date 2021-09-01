87899c79ad7498dc11e2b2f8a1083184d83921e8
David Burrows <david.burrows@starlingbank.com>
2021-09-01 17:21:07 +0100

    basic

---
+++ src/pages/page-1.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Link } from "gatsby";
+
+// markup
+const Page1 = () => {
+  return (
+    <main>
+      <title>Page One</title>
+      <h1>Page One</h1>
+      <Link to="/">Home</Link>
+    </main>
+  );
+};
+
+export default Page1;
---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const Page2 = () => {
+  return (
+    <main>
+      <title>Page Two</title>
+      <h1>Page Two</h1>
+    </main>
+  );
+};
+
+export default Page2;
